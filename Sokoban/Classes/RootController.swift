85f8466f7db58743ed0dcdc68470f2e593d900fd
LViOS160 <iuliiakorn@outlook.com>
2017-01-18 12:07:51 +0200

    LVIOS-62. Created initial folder structure

---
+++ Sokoban/Classes/RootController.swift
@@ -0,0 +1,21 @@
+//
+//  RootController.swift
+//  Sokoban
+//
+//  Created by admin on 1/18/17.
+//
+//
+
+import UIKit
+
+class RootController: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
