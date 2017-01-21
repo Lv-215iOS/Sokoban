316bd06838a4188550efb0c269473d8ca58bea0e
Oleksandr <oklahoma0707@gmail.com>
2017-01-21 20:20:51 +0200

    Added Floor Cell Scene Detail

--- Sokoban/Classes/Controllers/Scene/FloorCell.swift
+++ Sokoban/Classes/Controllers/Scene/FloorCell.swift
@@ -9,15 +9,85 @@
 import UIKit
 
 class FloorCell: UIView {
-
+    
     override func draw(_ rect: CGRect) {
-        // 1. Get CG context
         let context = UIGraphicsGetCurrentContext()
         
-        // 2. Draw into context
-        context?.setFillColor(UIColor.blue.cgColor)
+        context!.setFillColor(UIColor(red: 0.4392, green: 0.4118, blue: 0.4118, alpha: 1.0).cgColor)
         context?.fill(rect)
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0, y: 0.04 * rect.height/4))
+        context?.addLine(to: CGPoint(x: rect.width, y: 0.04 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0, y: rect.height/4))
+        context?.addLine(to: CGPoint(x: rect.width, y: rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0, y: 2 * rect.height/4))
+        context?.addLine(to: CGPoint(x: rect.width, y: 2 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0, y: 3 * rect.height/4))
+        context?.addLine(to: CGPoint(x: rect.width, y: 3 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: rect.width / 2, y: 0))
+        context?.addLine(to: CGPoint(x: rect.width / 2, y: rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0.02 * rect.width / 2, y: 0))
+        context?.addLine(to: CGPoint(x: 0.02 * rect.width / 2, y: rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 2.25 * rect.width / 3, y: rect.height / 4))
+        context?.addLine(to: CGPoint(x: 2.25 * rect.width / 3, y: 2 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0.75 * rect.width / 3, y: rect.height / 4))
+        context?.addLine(to: CGPoint(x: 0.75 * rect.width / 3, y: 2 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: rect.width / 2, y: 2 * rect.height / 4))
+        context?.addLine(to: CGPoint(x: rect.width / 2, y: 3 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0.02 * rect.width / 2, y: 2 * rect.height / 4))
+        context?.addLine(to: CGPoint(x: 0.02 * rect.width / 2, y: 3 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 2.25 * rect.width / 3, y: 3 * rect.height / 4))
+        context?.addLine(to: CGPoint(x: 2.25 * rect.width / 3, y: 4 * rect.height / 4))
+        context?.strokePath()
+        
+        context?.setLineWidth((rect.width + rect.height)/100)
+        context?.setStrokeColor(UIColor(red: 0.2863, green: 0.0431, blue: 0, alpha: 1.0).cgColor)
+        context?.move(to: CGPoint(x: 0.75 * rect.width / 3, y: 3 * rect.height / 4))
+        context?.addLine(to: CGPoint(x: 0.75 * rect.width / 3, y: 4 * rect.height / 4))
+        context?.strokePath()
+        
     }
- 
-
+    
 }
