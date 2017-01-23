8471e27abc68d0841a9ce36aaa5d6ebc5c2c790e
Dmytro <Dmytro Barabash>
2017-01-23 19:21:06 +0200

    Add segue in PlaygroundController to control animation from PlaygroundController.swift

--- Sokoban/Classes/Controllers/PlaygroundController.swift
+++ Sokoban/Classes/Controllers/PlaygroundController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class PlaygroundController: UIViewController {
+    
+    var sceneController: SceneController? = nil
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SceneController" {
+            sceneController = segue.destination as? SceneController
+        }
+    }
+    
+    @IBAction func MoveAction(_ sender: UIButton) {
+        sceneController?.animatePlayer(title: sender.currentTitle!)
+    }
+    
+    
     @IBAction func backToMenu(_ sender: UIButton) {
         let alert = UIAlertController(title: "Back to menu", message: "Quit game?", preferredStyle: .alert)
         let OKAction = UIAlertAction(title: "OK", style: .default) { (_) in
