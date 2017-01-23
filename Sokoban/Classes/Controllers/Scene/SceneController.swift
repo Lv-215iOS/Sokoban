162605e1239723bcc99efe191d9f9f4e32494065
Dmytro <Dmytro Barabash>
2017-01-23 17:26:23 +0200

    Add animation for player

--- Sokoban/Classes/Controllers/Scene/SceneController.swift
+++ Sokoban/Classes/Controllers/Scene/SceneController.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class SceneController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var animationBtn: UIButton!
+    var player = PlayerCell()
+    
+    @IBAction func animationBtnClicked(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "👉":
+            animateImage(type: player.imageListRight)
+        case "👆":
+            animateImage(type: player.imageListUp)
+        case "👈":
+            animateImage(type: player.imageListLeft)
+        case "👇🏿":
+            animateImage(type: player.imageListDown)
+        default:
+            break
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        player.initPlayer()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func animateImage(type: [UIImage]) {
+        imageView.animationImages = type
+        imageView.animationDuration = 0.35
+        imageView.startAnimating()
+        delay(delay: 0.35) {
+            self.imageView.stopAnimating()
+        }
     }
-    */
-
+    
+    func delay(delay:Double, closure:@escaping ()->()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            closure()
+        }
+    }
 }
