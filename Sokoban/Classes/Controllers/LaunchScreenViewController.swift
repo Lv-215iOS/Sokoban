74bf6a9bcbeb64ff0122e3dbc03e544e876ab613
pasik_01 <pavlotcubylski@gmail.com>
2017-01-25 22:05:08 +0200

    launch screen created

--- Sokoban/Classes/Controllers/LaunchScreenViewController.swift
+++ Sokoban/Classes/Controllers/LaunchScreenViewController.swift
@@ -12,21 +12,32 @@ class LaunchScreenViewController: UIViewController {
     
     @IBOutlet weak var appTitle: UILabel!
     
+    @IBOutlet weak var groupName: UILabel!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         appTitle.center.y -= view.bounds.height
+        groupName.center.y += view.bounds.height
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 1.5, animations: {
+        UIView.animate(withDuration: 2.0, delay: 1.5, usingSpringWithDamping: 5.0, initialSpringVelocity: 8.0, options: [], animations: {
             
-            self.appTitle.center.y += self.view.bounds.height
-            
+            self.groupName.center.y -= self.view.bounds.height
             
         }, completion: {(finished:Bool) in
             
             self.whatControllerToOpen()
         })
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            
+            self.appTitle.center.y += self.view.bounds.height
+            self.appTitle.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            self.appTitle.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 2)
+
+            
+        }, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -43,14 +54,12 @@ class LaunchScreenViewController: UIViewController {
     {
         
         if defaults.string(forKey: "isAppAlreadyLaunchedOnce") != nil{
-            print("App already launched")
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let vc: UINavigationController = storyboard.instantiateViewController(withIdentifier: "secondViewController") as! UINavigationController
             self.present(vc, animated: true, completion: nil)
             
         } else {
             defaults.set(true, forKey: "isAppAlreadyLaunchedOnce")
-            print("App launched first time")
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let vc: FirstLaunchViewController = storyboard.instantiateViewController(withIdentifier: "firstViewController") as! FirstLaunchViewController
             self.present(vc, animated: true, completion: nil)
