f3ed2ac0889b6459afd881d6810367a70b01935e
Oleksiy Bilyi <oleksiybilyi@Oleksiys-Mac.local>
2017-01-26 16:38:36 +0200

    Added checkMark for selected user, showing global score for users.

--- Sokoban/Classes/Controllers/PlayersViewController.swift
+++ Sokoban/Classes/Controllers/PlayersViewController.swift
@@ -2,13 +2,16 @@
 //  PlayersViewController.swift
 //  Sokoban
 //
-//  Created by pasik_01 on 19.01.17.
+//  Created by Oleksiy Bilyi on 19.01.17.
 //
 //
 
 import UIKit
 
 class PlayersViewController: UIViewController {
+    
+    /// Index for selected player cell
+    var selectedIndex: Int = 1
 
     @IBOutlet weak var playersTableView: UITableView!
     
@@ -22,8 +25,11 @@ class PlayersViewController: UIViewController {
     /// adding player alert appears 
     @IBAction func addNewPlayerButtonTapped(_ sender: UIButton) {
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        playersTableView.reloadData()
+    }
 }
-
 
 // MARK: - UITableViewDataSource
 extension PlayersViewController: UITableViewDataSource {
@@ -36,39 +42,61 @@ extension PlayersViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        guard let player = PlayersProvider.getPlayers()?[indexPath.row]
-             else {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        guard let player = PlayersProvider.getPlayers()?[indexPath.row],
+              let cellTextLabel = cell.textLabel,
+              let cellDetailTextLabel = cell.detailTextLabel else {
                 return cell
         }
-        cell.textLabel?.text = player.name
+        // if player in current cell is currentPlayer, check cell
+        if player == PlayersProvider.currentPlayer {
+            cell.accessoryType = .checkmark
+            tableView.selectRow(at: indexPath, animated: false, scrollPosition: .bottom)
+        } else {
+            cell.accessoryType = .none
+        }
+        cellTextLabel.text = player.name
+        cellDetailTextLabel.text = "score - " + (player.score?.stringValue)!
         return cell
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "List of Players"
-    }
-    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let path = IndexPath(row: 0, section: 0)
         guard let playerToRemove = PlayersProvider.getPlayers()?[indexPath.row],
-            editingStyle == .delete else {
+              let cell = tableView.cellForRow(at: path),
+              let cellText = cell.textLabel?.text,
+              editingStyle == .delete else {
                 return
+        }
+        // If deleting last player in table, present alert
+        if tableView.numberOfRows(inSection: 0) == 1 {
+            let alert = UIAlertController(title: "Can't delete last user", message: nil, preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .cancel, handler: { (action) in })
+            alert.addAction(ok)
+            present(alert, animated: true)
+            return
         }
         PlayersProvider.deletePlayer(playerToRemove)
         playersTableView.deleteRows(at: [indexPath], with: .automatic)
+        cell.accessoryType = .checkmark
+        PlayersProvider.setCurrentPlayerWith(name: cellText)
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
 }
+
 // MARK: - UITableViewDelegate
 extension PlayersViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath)
-        cell?.accessoryType = .checkmark
+        guard let cellText = tableView.cellForRow(at: indexPath)?.textLabel?.text else {
+                return
+        }
+        selectedIndex = indexPath.row
+        tableView.reloadData()
+        PlayersProvider.setCurrentPlayerWith(name: cellText)
     }
-    
 }
 
