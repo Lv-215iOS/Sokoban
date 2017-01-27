d440c40197e77b56ef53b2e2439362121823ae9c
pasik_01 <pavlotcubylski@gmail.com>
2017-01-27 10:51:54 +0200

    added camera function to create avatar

--- Sokoban/Classes/Controllers/FirstLaunchViewController.swift
+++ Sokoban/Classes/Controllers/FirstLaunchViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class FirstLaunchViewController: UIViewController {
+class FirstLaunchViewController: UIViewController,
+    UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
     
     
+    @IBOutlet weak var imageDisplay: UIImageView!
     @IBOutlet weak var addPlayerTextField: UITextField!
     
     @IBOutlet weak var createNewPlayerButton: UIButton!
@@ -19,7 +22,7 @@ class FirstLaunchViewController: UIViewController {
     
     /// to open MenuViewController and change nickname
     @IBAction func createNewPlayerButtonTapped(_ sender: UIButton) {
-        PlayersProvider.addPlayerWith(name: addPlayerTextField.text!, score: 0.0, levelsScores: [0.0], photo:UIImage())
+        PlayersProvider.addPlayerWith(name: addPlayerTextField.text!, score: 0.0, levelsScores: [0.0], photo:imageDisplay.image!)
         
         if defaults.string(forKey: "isPlayerAlreadyCreated") != nil{
             _ = navigationController?.popViewController(animated: true)
@@ -31,6 +34,18 @@ class FirstLaunchViewController: UIViewController {
             self.present(vc, animated: true, completion: nil)
             
         }
+        
+    }
+    
+    @IBAction func CameraAction(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        
+        self.present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageDisplay.image = info[UIImagePickerControllerOriginalImage] as! UIImage?; dismiss(animated: true, completion: nil)
         
     }
     
