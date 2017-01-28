20fe1c58ddaa138819dda62c92cb1d8f1a90c809
Sofiia Lushchanets <sofiya067@gmail.com>
2017-01-28 22:11:20 +0200

    Implemented levelStateImage. 1. if level score 0.0 or level is not passed yet-> cross pic is shown, score label is "untried" 2. if level score is more than 0.0 -> checkmark pic is shown, score label shows received score and it is in green color

--- Sokoban/Classes/Controllers/LevelsController.swift
+++ Sokoban/Classes/Controllers/LevelsController.swift
@@ -48,24 +48,32 @@ class LevelsController: UIViewController, UITableViewDelegate, UITableViewDataSo
         
         cell.levelNameLabel.text = level.name
         
-//        DispatchQueue.global(qos: .utility).async {
-//            let levelSnapshot = self.createLevelSnapshot()
-//            
-//            // do something time consuming here
-//            
-//            DispatchQueue.main.async {
-//                // now update UI on main thread
-//                cell.levelSnapshotImage.image = levelSnapshot
-//            }
-//        }
+        //        DispatchQueue.global(qos: .utility).async {
+        //            let levelSnapshot = self.createLevelSnapshot()
+        //
+        //            // do something time consuming here
+        //
+        //            DispatchQueue.main.async {
+        //                // now update UI on main thread
+        //                cell.levelSnapshotImage.image = levelSnapshot
+        //            }
+        //        }
         
-        
-        
-        if indexPath.row < levelsScoresArray.count {
+        if indexPath.row < levelsScoresArray.count {//level score in levelScoreLabel
             let levelScore = levelsScoresArray[indexPath.row]
             cell.levelScoreLabel.text = String(describing: levelScore)
+            
+                if levelScore > 0.0 {//sets correct levelStateImage
+                    cell.levelStateImage.image = UIImage(named: "levelPassedImage")
+                    cell.levelScoreLabel.textColor = UIColor.green
+                } else if levelScore == 0.0 {//sets correct levelStateImage
+                    cell.levelScoreLabel.text = "Untried"
+                    cell.levelStateImage.image = UIImage(named: "levelNotPassedImage")
+                }
+            
         } else {
             cell.levelScoreLabel.text = "Untried"
+            cell.levelStateImage.image = UIImage(named: "levelNotPassedImage")
         }
         
         return cell
@@ -98,14 +106,14 @@ class LevelsController: UIViewController, UITableViewDelegate, UITableViewDataSo
     
     //CALL THIS in SEPARATE THREAD in cellForRowAt
     
-//    func createLevelSnapshot() -> UIImage {
-//        
-//        UIGraphicsBeginImageContext((levelScene?.scene.frame.size)!)
-//        levelScene?.scene.layer.render(in: UIGraphicsGetCurrentContext()!)
-//        let snapshot = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        return snapshot!
-//        
-//    }
+    //    func createLevelSnapshot() -> UIImage {
+    //
+    //        UIGraphicsBeginImageContext((levelScene?.scene.frame.size)!)
+    //        levelScene?.scene.layer.render(in: UIGraphicsGetCurrentContext()!)
+    //        let snapshot = UIGraphicsGetImageFromCurrentImageContext()
+    //        UIGraphicsEndImageContext()
+    //        return snapshot!
+    //        
+    //    }
     
 }
