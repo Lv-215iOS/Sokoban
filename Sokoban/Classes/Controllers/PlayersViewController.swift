d7197cad1d47622d30b365a7b3d36c2cb7ffb823
Oleksiy Bilyi <oleksiybilyi@Oleksiys-Mac.local>
2017-01-29 23:34:07 +0200

    Added activity indicator for cell and asynchronously loading photos for players

--- Sokoban/Classes/Controllers/PlayersViewController.swift
+++ Sokoban/Classes/Controllers/PlayersViewController.swift
@@ -10,54 +10,70 @@ import UIKit
 
 class PlayersViewController: UIViewController {
     
+    // MARK: - Properties
     fileprivate let playerCellIdentifier = "playerCellReuseIdentifier"
-    
+    fileprivate let playersResultsController = PlayersProvider.fetchedResultController
     /// Index for selected player cell
     var selectedIndex: Int = 1
 
+    // MARK: - IBOutlets
     @IBOutlet weak var playersTableView: UITableView!
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        //playersTableView.register(CustomPlayerCell.self, forCellReuseIdentifier: playerCellIdentifier)
+        
+        PlayersProvider.fetchedResultController.delegate = self
         playersTableView.dataSource = self
         playersTableView.delegate = self
-    }
-    
-    /// adding player alert appears 
-    @IBAction func addNewPlayerButtonTapped(_ sender: UIButton) {
     }
     
     override func viewWillAppear(_ animated: Bool) {
         playersTableView.reloadData()
     }
+    
+    // MARK: - IBActions
+    /// adding player alert appears 
+    @IBAction func addNewPlayerButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
 // MARK: - UITableViewDataSource
 extension PlayersViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let players = PlayersProvider.getPlayers() else {
-            return 1
+        guard let sectionInfo = PlayersProvider.fetchedResultController.sections?[section] else {
+            return 0
         }
-        return players.count
+        return sectionInfo.numberOfObjects
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: playerCellIdentifier, for: indexPath) as! CustomPlayerCell
-        guard let player = PlayersProvider.getPlayers()?[indexPath.row] else {
-                return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: playerCellIdentifier, for: indexPath)
+        guard let playerCell = cell as? CustomPlayerCell else {
+            return cell
         }
+        let player = PlayersProvider.fetchedResultController.object(at: indexPath)
         // if player in current cell is currentPlayer, check cell
         if player == PlayersProvider.currentPlayer {
-            cell.accessoryType = .checkmark
+            playerCell.accessoryType = .checkmark
+            selectedIndex = indexPath.row
             tableView.selectRow(at: indexPath, animated: false, scrollPosition: .bottom)
         } else {
-            cell.accessoryType = .none
+            playerCell.accessoryType = .none
         }
-        cell.playerName.text = player.name
-        cell.playerScore.text = "score - " + (player.score?.stringValue)!
-        return cell
+        PlayersProvider.asynchGetPhotoForPlayer(player.name!) { (image) in
+            DispatchQueue.main.sync {
+                playerCell.playerImageView.image = image
+                playerCell.removeActivityIndicator()
+            }
+        }
+        playerCell.playerName.text = player.name
+        if let playerScore = player.score?.stringValue {
+            playerCell.playerScore.text = "score - " + playerScore
+        }
+        return playerCell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
@@ -94,12 +110,38 @@ extension PlayersViewController: UITableViewDataSource {
 extension PlayersViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let cellText = tableView.cellForRow(at: indexPath)?.textLabel?.text else {
+        guard let currentSelectedCell = tableView.cellForRow(at: indexPath) as? CustomPlayerCell,
+              let checkedCell = tableView.cellForRow(at: IndexPath(row: selectedIndex, section: 0)) else {
                 return
         }
+        currentSelectedCell.accessoryType = .checkmark
+        if selectedIndex != indexPath.row {
+            checkedCell.accessoryType = .none
+        }
         selectedIndex = indexPath.row
-        tableView.reloadData()
-        PlayersProvider.setCurrentPlayerWith(name: cellText)
+        if let currentSelectedPlayerName = currentSelectedCell.playerName.text {
+            PlayersProvider.setCurrentPlayerWith(name: currentSelectedPlayerName)
+        }
     }
+}
+
+// MARK: - NSFetchedResultsControllerDelegate
+extension PlayersViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        playersTableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        if type == .update {
+            let cell = playersTableView.cellForRow(at: indexPath!) as! CustomPlayerCell
+            cell.playerImageView.image = UIImage(data: PlayersProvider.fetchedResultController.object(at: indexPath!).photo!)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        playersTableView.endUpdates()
+    }
+    
 }
 
