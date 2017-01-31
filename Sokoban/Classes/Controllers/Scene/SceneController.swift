bf89abebf86299d2e97abeb5ee998a5521c9c1e9
Dmytro <Dmytro Barabash>
2017-01-31 13:47:53 +0200

    Change game view logic to game string logic in SceneController, SceneBuilder. Updater BlockCellIn, BlockCellOut, PlayerCell, FloorCell. Change Dot view.

--- Sokoban/Classes/Controllers/Scene/SceneController.swift
+++ Sokoban/Classes/Controllers/Scene/SceneController.swift
@@ -2,7 +2,7 @@
 //  SceneController.swift
 //  Sokoban
 //
-//  Created by admin on 1/18/17.
+//  Created by Dmytro on 1/18/17.
 //
 //
 
@@ -15,6 +15,8 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
     
     var currentLevel: Level?
     var matrix: String!
+    var indexBlock: [Int] = []
+    var temp = 0
     
     var sceneBuilder = SceneBuilder()
     @IBOutlet weak var scrollView: UIScrollView!
@@ -62,6 +64,13 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
     
     override func viewDidLoad() {
         matrix = currentLevel?.scene?.matrix
+        for i in 0..<matrix.characters.count {
+            let index = matrix.index(matrix.startIndex, offsetBy: i)
+            print("mrx = \(matrix[index])")
+            if matrix[index] == "%" {
+                indexBlock.append(i)
+            }
+        }
     }
     
     /// animate players moves
@@ -98,6 +107,11 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
         let y_pos = getPosition(str: matrix!, findElement: "&")! / sceneWidth!
         let x_pos = getPosition(str: matrix!, findElement: "&")! - sceneWidth! * y_pos
         
+        if isFinish() {
+            
+        }
+        
+        
         if isAbleToMove(x: x_pos, y: y_pos, move_x: x, move_y: y) {
 //            print(getSymbol(x: x_pos + x, y: y_pos + y))
             if getSymbol(x: x_pos + x, y: y_pos + y) == "%" {
@@ -114,7 +128,7 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
     }
     
     func isAbleToMove(x: Int, y: Int, move_x: Int, move_y: Int) -> Bool {
-        return !isWall(near: x, y: y, move_x: move_x, move_y: move_y)
+        return !(isWall(near: x, y: y, move_x: move_x, move_y: move_y))
     }
     
     /// check if next step is wall (wall near player, wall near block)
@@ -122,6 +136,7 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
         return getSymbol(x: x + move_x, y: y + move_y) == "#"
     }
     
+    //TODO:
     /// check if next step is block (block near block)
     func isBlock(near x: Int, y: Int, move_x: Int, move_y: Int) -> Bool {
         return getSymbol(x: x + move_x, y: y + move_y) == "%"
@@ -151,17 +166,21 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
         }
     }
     
-    func getPosition(str :String, findElement: Character, block_index: Int = 0) -> Int? {
-        var str = str
+    func getPosition(str :String, findElement: Character, block_value: Int = 0) -> Int? {
+//        var str = str
         for (index, value) in Array(str.characters).enumerated() {
             if value == findElement {
-                if block_index > 0 {
-                    for i in 0...index {
-                        str.remove(at: str.startIndex)
-                    }
-                    return index + 1 + getPosition(str: str, findElement: findElement, block_index: block_index - 1)!
+//                if block_index > 0 {
+//                    for i in 0...index {
+//                        str.remove(at: str.startIndex)
+//                    }
+//                    return index + 1 + getPosition(str: str, findElement: findElement, block_index: block_index - 1)!
+//                }
+                if block_value == 0 {
+                    return index
+                } else if index == block_value {
+                    return index
                 }
-                return index
             }
         }
         return nil
@@ -197,21 +216,45 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
             index = matrix?.index((matrix?.startIndex)!, offsetBy: i)
             print(String(describing: matrix?[index]))
             if String(describing: matrix![index]) == "%" {
-                block_index += 1
+                block_index = indexBlock.index(of: i)!
+            }
+            if i == block {
+                index = matrix?.index((matrix?.startIndex)!, offsetBy: i + sceneWidth! * y + x)
+                if matrix?[index] == "#" || matrix?[index] == "%" {
+                    return false
+                }
+                temp = indexBlock[block_index]
+                indexBlock[block_index] += sceneWidth! * y + x
             }
         }
         
         
-        print(getPosition(str: matrix!, findElement: "%", block_index: block_index))
-        let y_pos = getPosition(str: matrix!, findElement: "%", block_index: block_index)! / sceneWidth!
-        let x_pos = (getPosition(str: matrix!, findElement: "%", block_index: block_index)! - sceneWidth! * y_pos)
+        print(getPosition(str: matrix!, findElement: "%", block_value: temp))
+        let y_pos = getPosition(str: matrix!, findElement: "%", block_value: temp)! / sceneWidth!
+        let x_pos = (getPosition(str: matrix!, findElement: "%", block_value: temp)! - sceneWidth! * y_pos)
         
         if isAbleToMove(x: x_pos, y: y_pos, move_x: x, move_y: y) {
+            
+            sceneBuilder.blockCellOut[block_index].isHidden = false
+            let blockIn = findBlockIn(center: sceneBuilder.blockCellOut[block_index].center)
+            blockIn.isHidden = true
+            
             UIView.animate(withDuration: 0.35) {
                 self.sceneBuilder.blockCellOut[block_index].center.x += CGFloat(x) * self.sceneBuilder.blockCellOut[block_index].bounds.size.width
                 self.sceneBuilder.blockCellOut[block_index].center.y += CGFloat(y) * self.sceneBuilder.blockCellOut[block_index].bounds.size.height
                 self.swapSymbol(x: x_pos, y: y_pos, x_next: x_pos + x, y_next: y_pos + y)
+                
+                index = self.matrix?.index((self.matrix?.startIndex)!, offsetBy: self.indexBlock[block_index])
+                
+                if self.isBlockOnDot(block: self.sceneBuilder.blockCellOut[block_index]) {
+                    self.delay(delay: 0.25) {
+                        self.sceneBuilder.blockCellOut[block_index].isHidden = true
+                        let blockIn = self.findBlockIn(center: self.sceneBuilder.blockCellOut[block_index].center)
+                        blockIn.isHidden = false
+                    }
+                }
             }
+            
             return true
         } else {
             return false
@@ -269,13 +312,19 @@ class SceneController: UIViewController, UIScrollViewDelegate, SceneControllerIn
         return false
     }
     
-    func findBlockIn(x: CGFloat, y: CGFloat) -> BlockCellIn {
+    func findBlockIn(center: CGPoint) -> BlockCellIn {
         for block in sceneBuilder.blockCellIn {
-            if block.center.x == x && block.center.y == y {
+            if block.center.x == center.x && block.center.y == center.y {
                 return block
             }
         }
         return sceneBuilder.blockCellIn.last!
+//        for block in sceneBuilder.blockCellIn {
+//            if block.center.x == x && block.center.y == y {
+//                return block
+//            }
+//        }
+//        return sceneBuilder.blockCellIn.last!
     }
     
     func isFinish() -> Bool {
