ca943afc1d1b2cae42e64b673e83cf3c84ce6e21
adminaccount <adminaccount@macminissu2.training.local>
2017-01-31 15:38:12 +0200

    Score is calculated

--- Sokoban/Classes/Controllers/PlaygroundController.swift
+++ Sokoban/Classes/Controllers/PlaygroundController.swift
@@ -24,6 +24,11 @@ class PlaygroundController: UIViewController {
     var time = 0
     var movesCount = 0
     
+    var minMoves = 1.0
+    var minTime = 1.0
+    var timeInSecs = 0.0
+    var score = 0.0
+    
     
     var sceneController: SceneController? = nil
         
@@ -67,11 +72,24 @@ class PlaygroundController: UIViewController {
         
     }
     
+    func ifTheEndOfLevel() {
+        if isPlaying {
+            timer.invalidate()
+            isPlaying = false
+        }
+        calculateScore()
+    }
+
+    func calculateScore() {
+        score = ((minTime/timeInSecs)*(minMoves/Double(movesCount))) * 100
+    }
+
     func updateTime() {
         time += 1
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
         timeLabel.text = String(format:"%02i:%02i", minutes, seconds)
+        timeInSecs = Double(minutes * 60 + seconds)
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
